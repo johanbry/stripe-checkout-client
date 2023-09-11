42d695072fd10a7b1285d471c3c407d2c41d0dde
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-09-11 13:23:39 +0200

    context for auth, login, logout, register

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,81 @@
+import {
+  createContext,
+  useContext,
+  useState,
+  PropsWithChildren,
+  useEffect,
+} from "react";
+
+interface IUser {
+  stripeId: string;
+  name: string;
+  email: string;
+}
+
+interface IUserContext {
+  user: IUser | null;
+  errorMessage: string | null;
+  login: (email: string, password: string) => void;
+  logout: () => void;
+}
+
+export const UserContext = createContext<IUserContext>(null as any);
+
+export const useUserContext = () => useContext(UserContext);
+
+const UserProvider = ({ children }: PropsWithChildren) => {
+  const [user, setUser] = useState<IUser | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    const authorize = async () => {
+      try {
+        const res = await fetch("/api/users/authorize");
+        if (!res.ok) throw new Error(res.status.toString());
+        const data = await res.json();
+        setUser(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    authorize();
+  }, []);
+
+  const login = async (email: string, password: string) => {
+    try {
+      const res = await fetch("/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+      if (!res.ok) throw new Error(res.status.toString());
+      const data = await res.json();
+      setUser(data);
+      setErrorMessage(null);
+    } catch (error) {
+      console.log(error);
+
+      setErrorMessage("Failed to login. " + error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await fetch("/api/users/logout");
+      setUser(null);
+    } catch (error) {
+      console.log("Error when logging out on server");
+    }
+  };
+
+  return (
+    <UserContext.Provider value={{ user, errorMessage, login, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserProvider;
