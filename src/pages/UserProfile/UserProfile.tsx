773476dc44d5f7d6a9aed845151edd06271e21c9
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-09-11 13:24:30 +0200

    show logged in user info and logout button

--- src/pages/UserProfile/UserProfile.tsx
+++ src/pages/UserProfile/UserProfile.tsx
@@ -1,7 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import { useUserContext } from "../../context/UserContext";
+
 type Props = {};
 
 const UserProfile = (props: Props) => {
-  return <div>UserProfile</div>;
+  const { user, logout } = useUserContext();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+  return (
+    <div>
+      <h3>User information:</h3> Name: {user?.name}
+      <br />
+      Email: {user?.email}
+      <button type="button" onClick={handleLogout}>
+        Logga ut
+      </button>
+    </div>
+  );
 };
 
 export default UserProfile;
