e7bfac9352bd40aa3d636711105035b0b2fad768
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-09-11 17:11:55 +0200

    register logics and infomessage

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -16,7 +16,14 @@ interface IUser {
 interface IUserContext {
   user: IUser | null;
   errorMessage: string | null;
-  login: (email: string, password: string, redirect?: string) => void;
+  infoMessage: string | null;
+  register: (
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string
+  ) => void;
+  login: (email: string, password: string, redirect: string) => void;
   logout: () => void;
 }
 
@@ -28,6 +35,7 @@ const UserProvider = ({ children }: PropsWithChildren) => {
   const [user, setUser] = useState<IUser | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [infoMessage, setInfoMessage] = useState<string | null>(null);
 
   const navigate = useNavigate();
 
@@ -45,8 +53,53 @@ const UserProvider = ({ children }: PropsWithChildren) => {
     authorize();
   }, []);
 
-  const login = async (email: string, password: string, redirect: string) => {
+  useEffect(() => {
+    console.log("innan timer");
+    const timer = setTimeout(() => {
+      setInfoMessage(null);
+    }, 5000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [infoMessage]);
+
+  const register = async (
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string
+  ) => {
     try {
+      setInfoMessage(null);
+      setErrorMessage(null);
+      const res = await fetch("/api/users/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ firstName, lastName, email, password }),
+      });
+      if (!res.ok) throw new Error(res.status.toString());
+      //const data = await res.json();
+      //await login(email, password, "/userprofile");
+      navigate("/login");
+      setInfoMessage("Welcome! Your account is created, please log in.");
+    } catch (error) {
+      console.log(error);
+
+      setErrorMessage("Failed to register. " + error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const login = async (
+    email: string,
+    password: string,
+    redirect: string | null = null
+  ) => {
+    try {
+      setInfoMessage(null);
+      setErrorMessage(null);
       const res = await fetch("/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -55,7 +108,7 @@ const UserProvider = ({ children }: PropsWithChildren) => {
       if (!res.ok) throw new Error(res.status.toString());
       const data = await res.json();
       setUser(data);
-      setErrorMessage(null);
+      setInfoMessage("You are now logged in!");
       if (redirect) navigate(redirect);
     } catch (error) {
       console.log(error);
@@ -70,13 +123,16 @@ const UserProvider = ({ children }: PropsWithChildren) => {
     try {
       await fetch("/api/users/logout");
       setUser(null);
+      setInfoMessage("You are now logged out!");
     } catch (error) {
       console.log("Error when logging out on server");
     }
   };
 
   return (
-    <UserContext.Provider value={{ user, errorMessage, login, logout }}>
+    <UserContext.Provider
+      value={{ user, errorMessage, infoMessage, register, login, logout }}
+    >
       {children}
     </UserContext.Provider>
   );
