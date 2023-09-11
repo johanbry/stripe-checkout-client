cd404910b0f470ef199323714756048dd49433f5
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-09-11 13:24:03 +0200

    login logics form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,41 @@
+import { FormEvent, useState } from "react";
+import { useUserContext } from "../../context/UserContext";
+
 type Props = {};
 
 const Login = (props: Props) => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const { login, errorMessage } = useUserContext();
+
+  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("handlelogin", email, password);
+
+    login(email, password);
+  };
+
+  return (
+    <>
+      <form onSubmit={(e) => handleLogin(e)}>
+        <input
+          type="text"
+          value={email}
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          value={password}
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Login</button>
+      </form>
+      {errorMessage && <p>{errorMessage}</p>}
+    </>
+  );
 };
 
 export default Login;
