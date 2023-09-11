7c3d7df791e62b26194fbd376a3e97b416881d06
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-09-11 13:22:43 +0200

    UserProvider added for context

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,7 @@ import Register from "./pages/Register/Register";
 import UserProfile from "./pages/UserProfile/UserProfile";
 
 import "./index.css";
+import UserProvider from "./context/UserContext";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -32,6 +33,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
   </React.StrictMode>
 );
