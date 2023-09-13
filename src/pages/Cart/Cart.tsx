6518c04c048637b9d8468bd43b58d8399db6b98f
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-09-13 14:29:44 +0200

    handle checkout logics

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,12 +1,19 @@
+import { useSearchParams } from "react-router-dom";
 import { useCartContext } from "../../context/CartContext";
+import { useUserContext } from "../../context/UserContext";
 
 import { ICartItem } from "./../../context/CartContext";
+import { useState } from "react";
 
 type Props = {};
 
 const Cart = (props: Props) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
   const { cartItems, subtractFromCart, addToCart, cartTotal } =
     useCartContext();
+  const { user } = useUserContext();
 
   const handleSubtract = (id: string) => {
     subtractFromCart(id, 1);
@@ -16,11 +23,35 @@ const Cart = (props: Props) => {
     addToCart(item, 1);
   };
 
+  const handleCheckout = async () => {
+    const createParams = {
+      cart: cartItems,
+      customer: user?.stripeId,
+    };
+    try {
+      setErrorMessage(null);
+      setIsLoading(true);
+      const res = await fetch("/api/checkout/create-stripe-session", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(createParams),
+      });
+      if (!res.ok) throw Error;
+      const session = await res.json();
+      window.location.href = session.url;
+    } catch (error) {
+      setErrorMessage("Could not proceed to checkout.");
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div>
       {cartItems.map((item) => (
         <div key={item.id}>
-          {item.qty} x {item.name}[{item.id}] á {item.price}{" "}
+          {item.qty} x {item.name}[{item.id}] á {item.price} - {item.price_id}
           <button type="button" onClick={() => handleSubtract(item.id)}>
             -
           </button>
@@ -30,6 +61,8 @@ const Cart = (props: Props) => {
         </div>
       ))}
       <p>Total: {cartTotal()}</p>
+      {user && <button onClick={handleCheckout}>Checkout</button>}
+      {!user && <p>Log in to proceed to checkout!</p>}
     </div>
   );
 };
