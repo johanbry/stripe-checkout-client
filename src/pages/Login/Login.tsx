7dafaf3feea95679795050951854cf8d7e807a76
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-09-22 13:29:04 +0200

    reset error message in user context on mount

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -6,7 +6,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 const Login = () => {
-  const { user, login, isLoading, errorMessage } = useUserContext();
+  const { user, login, isLoading, errorMessage, setErrorMessage } =
+    useUserContext();
   const navigate = useNavigate();
 
   const onSubmit: SubmitHandler<ILoginUser> = async (data: ILoginUser) => {
@@ -17,11 +18,15 @@ const Login = () => {
     if (user) navigate("/userprofile");
   }, [user, navigate]);
 
+  useEffect(() => {
+    setErrorMessage(null);
+  }, [setErrorMessage]);
+
   return (
     <div className="box-container form-wrapper">
       <h1>Logga in</h1>
       <LoginForm isLoading={isLoading} onSubmit={onSubmit} />
-      {errorMessage && <p>{errorMessage}</p>}
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
       <div className="toggle-link">
         <Link to="/register">Registrera dig</Link>
       </div>
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -3,9 +3,11 @@ import { useUserContext } from "../../context/UserContext";
 import { IRegisterUser } from "./../../interfaces/interfaces";
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 const Register = () => {
-  const { register, errorMessage, isLoading } = useUserContext();
+  const { register, errorMessage, setErrorMessage, isLoading } =
+    useUserContext();
 
   const onSubmit: SubmitHandler<IRegisterUser> = async (
     data: IRegisterUser
@@ -19,11 +21,15 @@ const Register = () => {
     );
   };
 
+  useEffect(() => {
+    setErrorMessage(null);
+  }, [setErrorMessage]);
+
   return (
     <div className="box-container form-wrapper">
       <h1>Registrera</h1>
       <RegisterForm isLoading={isLoading} onSubmit={onSubmit} />
-      {errorMessage && <p>{errorMessage}</p>}
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
       <div className="toggle-link">
         <Link to="/login">Logga in</Link>
       </div>
