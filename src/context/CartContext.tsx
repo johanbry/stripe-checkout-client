8ed62647104530f883a98230d1722584b7485abb
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-09-22 12:14:12 +0200

    move interfaces, default context values

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,37 +1,32 @@
-import { PropsWithChildren, createContext, useContext, useState } from "react";
+import {
+  PropsWithChildren,
+  createContext,
+  useCallback,
+  useContext,
+} from "react";
 
-export interface IProduct {
-  id: string;
-  name: string;
-  description: string;
-  default_price: {
-    id: string;
-    unit_amount: number;
-  };
-  price: number;
-  price_id: string;
-  images: string[];
-}
+import { useLocalStorage } from "../components/hooks/useLocalStorage";
+import { ICartContext, ICartItem, IProduct } from "../interfaces/interfaces";
 
-export interface ICartItem extends IProduct {
-  qty: number;
-}
+const defaultValues = {
+  cartItems: [],
+  addToCart: () => {},
+  subtractFromCart: () => {},
+  clearCart: () => {},
+  qtyInCart: () => {
+    return 0;
+  },
+  cartTotal: () => {
+    return 0;
+  },
+};
 
-interface ICartContext {
-  cartItems: ICartItem[];
-  addToCart: (product: IProduct, qty: number) => void;
-  subtractFromCart: (id: string, qty: number) => void;
-  clearCart: () => void;
-  qtyInCart: () => number;
-  cartTotal: () => number;
-}
-
-export const CartContext = createContext<ICartContext>(null as any);
+export const CartContext = createContext<ICartContext>(defaultValues);
 
 export const useCartContext = () => useContext(CartContext);
 
 const CartProvider = ({ children }: PropsWithChildren) => {
-  const [cartItems, setCartItems] = useState<ICartItem[]>([]);
+  const [cartItems, setCartItems] = useLocalStorage<ICartItem[]>("cart", []);
 
   const addToCart = (product: IProduct, qty: number) => {
     const itemIndex = cartItems.findIndex((item) => item.id === product.id);
@@ -56,9 +51,9 @@ const CartProvider = ({ children }: PropsWithChildren) => {
     setCartItems(newCartItems);
   };
 
-  const clearCart = () => {
+  const clearCart = useCallback(() => {
     setCartItems([]);
-  };
+  }, [setCartItems]);
 
   const qtyInCart = () => {
     return cartItems.reduce((sum: number, item) => sum + item.qty, 0);
